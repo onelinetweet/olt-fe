add82ac3b6b0134d8710b4dc993abc14f14a09cb
dambar <notdambar@gmail.com>
2022-11-27 03:27:14 +0800

    init

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+console.log(analytics)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
